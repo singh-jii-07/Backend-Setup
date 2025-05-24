21d1eafdc6f1aab2ed5202c289410cfd6abe346d
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-05-24 18:24:51 -0500

    User model add

---
+++ src/Models/User.model.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+const Userschema = new schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.type.object,
+        ref: "video",
+      },
+    ],
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+export const User = mongoose.model("User", Userschema);
